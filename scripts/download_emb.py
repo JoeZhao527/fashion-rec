c0003e92749fbdce68b89c558af5347c5d06442a
haokai-jojo <haokai@biomap.com>
2024-08-01 14:51:58 +1000

    download feature script

---
+++ scripts/download_emb.py
@@ -0,0 +1,47 @@
+import requests
+import os
+
+def download_file_from_google_drive(file_id, destination, url):
+    session = requests.Session()
+
+    response = session.get(url, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(url, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    feat_dir = "./feature"
+    if not os.path.exists(feat_dir):
+        os.makedirs(feat_dir)
+
+    f_name = 'dino_image_emb.npy'
+    download_file_from_google_drive(
+        'dino_image_emb.npy', os.path.join(feat_dir, f_name),
+        url="https://drive.google.com/file/d/1BACmF4PBhy-RmgSsj0GgZjJgcPZUXfND/view?usp=sharing"
+    )
+
+    f_name = 'glove_text_emb.npy'
+    download_file_from_google_drive(
+        'glove_text_emb.npy', os.path.join(feat_dir, f_name),
+        url="https://drive.google.com/file/d/1-w1A5_wPs1ROu5gRXtqylPtmFG2eXDpX/view?usp=sharing"
+    )
+    
+    print(f"File downloaded to {feat_dir}")
